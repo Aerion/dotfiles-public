8aae875e648cc143ff221e3d22c0f6858e10863b
Guillaume Taquet Gasperini <guillaume.taquetgasperini@epita.fr>
2019-05-14 17:08:17 +0000

    emacs: add markdown-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -195,6 +195,14 @@ There are two things you can do about this warning:
   :config
   (add-to-list 'company-backends 'company-omnisharp))
 
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 ;;; Backups
 (defvar backup-directory "~/.emacs-backups")
 (if (not (file-exists-p backup-directory))
