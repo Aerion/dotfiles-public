a08c2f1f97a193f15578d7fdc5764364ac6e2680
Guillaume Taquet Gasperini <perso@aerion.me>
2020-04-14 19:15:05 +0200

    fish: untabify prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,27 +1,28 @@
 # ~/dotfiles τ cd -                                                *master +21
 
 function fish_prompt
-	set -l symbol "τ "
-	set -l code $status
+    set -l symbol "τ "
+    set -l code $status
 
-	if test "$theme_complete_path" = "yes"
-		set cwd (prompt_pwd)
-	else
-		set cwd (basename (prompt_pwd))
 
-		if git::is_repo
-			set root_folder (command git rev-parse --show-toplevel ^/dev/null)
-			set parent_root_folder (dirname $root_folder)
-			set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-		end
-	end
+    if test "$theme_complete_path" = "yes"
+        set cwd (prompt_pwd)
+    else
+        set cwd (basename (prompt_pwd))
 
-	printf (cyan)"$cwd "(off)
+        if git::is_repo
+            set root_folder (command git rev-parse --show-toplevel ^/dev/null)
+            set parent_root_folder (dirname $root_folder)
+            set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+        end
+    end
 
-	if test "$code" != 0
-	    echo -n -s (red)"($code) $symbol"(off)
-        else
-            printf "$symbol"
-	end
+    printf (cyan)"$cwd "(off)
+
+    if test "$code" != 0
+        echo -n -s (red)"($code) $symbol"(off)
+    else
+        printf "$symbol"
+    end
 
 end
