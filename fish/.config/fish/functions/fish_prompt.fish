5fcef7896c072abe6d8c1a00ffee91921cc275ab
Guillaume Taquet Gasperini <perso@aerion.me>
2020-05-06 20:22:28 +0200

    fish: use fish git functions in prompt instead of custom ones

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,29 +1,25 @@
 # ~/dotfiles τ cd -                                                *master +21
 
 function fish_prompt
+    set -l red "d75f5f"
+    set -l cyan "86d8fa"
     set -l symbol "τ "
     set -l code $status
 
     if set -q fish_private_mode
-        printf "🕵  "
+        echo -n "🕵  "
     end
 
     if test "$theme_complete_path" = "yes"
         set cwd (prompt_pwd)
     else
         set cwd (basename (prompt_pwd))
-
-        if git::is_repo
-            set root_folder (command git rev-parse --show-toplevel ^/dev/null)
-            set parent_root_folder (dirname $root_folder)
-            set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-        end
     end
 
-    printf (cyan)"$cwd "(off)
+    echo -n -s (set_color -o $cyan)"$cwd "(set_color -o normal)
 
     if test "$code" != 0
-        echo -n -s (red)"($code) $symbol"(off)
+        echo -n -s (set_color -o $red)"($code) $symbol"(set_color -o normal)
     else
         printf "$symbol"
     end
