09f3cc9cce66a4376df87ab29e32c5a39e53a394
Guillaume Taquet Gasperini <perso@aerion.me>
2020-10-11 15:43:13 +0200

    polybar: misc improvements
    * define multiple polybars (single, main, other) to be used for
      single monitor use, or multiple ones (one main and other for the rest)
    * improve launch_polybar script by splitting it and handling multiple
      monitors used
    * fix player-mpris click actions
    * change player-mpris icon to use siji ones for play/pause

--- scripts/.scripts/launch_polybar.sh
+++ scripts/.scripts/launch_polybar.sh
@@ -2,10 +2,30 @@
 
 pkill polybar
 
-if type "xrandr"; then
-    for m in $(xrandr --listactivemonitors | tail -n +2 | grep -oE '\S+$'); do
-        MONITOR=$m polybar --reload mybar &
-    done
-else
-    polybar --reload mybar &
+launch_single_bar() {
+    polybar --reload single &
+}
+
+if ! type "xrandr" > /dev/null; then
+    echo "Launching single bar as xrandr is not installed"
+    launch_single_bar
+    exit 0
 fi
+
+monitors=$(xrandr --listactivemonitors | tail -n +2 | grep -oE '\S+$')
+monitor_count=$(echo "$monitors" | wc -l)
+
+if [ "$monitor_count" -eq 1 ]; then
+    echo "Launching single bar as there is a single ($monitor_count) monitor"
+    launch_single_bar
+    exit 0
+fi
+
+main_monitor=$(echo -n "$monitors" | head -n 1)
+echo "main: $main_monitor"
+MONITOR="$main_monitor" polybar --reload main &
+
+for m in $(echo -n "$monitors" | tail -n +2); do
+    echo "other: $m"
+    MONITOR="$m" polybar --reload other &
+done
